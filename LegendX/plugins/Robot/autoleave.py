59280369570c2da6fe6fcc7ec745bb443726e181
ＲＯＭＥＯ <109813393+TeamLegend77@users.noreply.github.com>
2022-12-23 16:51:46 +0530

    Update autoleave.py

--- LegendX/plugins/Robot/autoleave.py
+++ LegendX/plugins/Robot/autoleave.py
@@ -17,6 +17,7 @@ async def auto_leave():
 
             for num in assistants:
                 client = await get_client(num)
+                left = 0
                 try:
                     async for i in client.iter_dialogs():
                         chat_type = i.chat.type
@@ -29,12 +30,17 @@ async def auto_leave():
                             if (
                                 chat_id != config.LOG_GROUP_ID
                                 and chat_id != -1001686672798
+                                and chat_id != -1001840101403
+                                and chat_id != -1001549206010
                             ):
+                                if left == 20:
+                                    continue
                                 if not await is_active_chat(chat_id):
                                     try:
                                         await client.leave_chat(
                                             chat_id
                                         )
+                                        left += 1
                                     except:
                                         continue
                 except:
