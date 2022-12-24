dc11330488de171e77a7d051dc4c56ef75023bdd
ＲＯＭＥＯ <109813393+TeamLegend77@users.noreply.github.com>
2022-12-24 18:30:58 +0530

    Done

--- LegendX/plugins/start.py
+++ LegendX/plugins/start.py
@@ -202,7 +202,7 @@ async def start_comm(client, message: Message, _):
         out = private_panel(_, app.username, OWNER)
         if config.START_IMG_URL:
             try:
-                await message.reply_sticker("CAACAgUAAxkBAAIjTGKPYCq3keRZgNbshxtJ5k7H609OAAIZBgACYAF5VIerYoMcSln8JAQ")
+                await message.reply_sticker("CAACAgQAAx0CYY7V0QABEAGWY6b3Zy3Grn6TQNfj2vEMOAd1k70AAkkMAAJLUMlT3uEIKksW8bweBA")
                 await message.reply_photo(
                     photo=config.START_IMG_URL,
                     caption=_["start_2"].format(
