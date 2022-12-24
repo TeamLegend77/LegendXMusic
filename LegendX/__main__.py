cc8827b911d79352413f767654a361592a92025f
ＲＯＭＥＯ <109813393+TeamLegend77@users.noreply.github.com>
2022-12-24 15:27:37 +0530

    Update __main__.py

--- LegendX/__main__.py
+++ LegendX/__main__.py
@@ -24,7 +24,7 @@ async def init():
         and not config.STRING5
     ):
         LOGGER("LegendX").error(
-            "No Assistant Clients Vars Defined!.. Exiting Process.."
+            "WTF Baby ! Atleast add a pyrogram string, How Cheap..."
         )
         return
     if (
@@ -32,7 +32,7 @@ async def init():
         and not config.SPOTIFY_CLIENT_SECRET
     ):
         LOGGER("LegendX").warning(
-            "No Spotify Vars defined. Your bot won't be able to play spotify queries."
+            "Sur spotify id aur secret toh daala hi nahi aapne ab toh spotify se nahi chala paaoge gaane."
         )
     try:
         users = await get_gbanned()
@@ -45,28 +45,32 @@ async def init():
         pass
     await app.start()
     for all_module in ALL_MODULES:
-        importlib.import_module("LegendX.plugins" + all_module)
+        importlib.import_module("LegendX.plugins." + all_module)
     LOGGER("LegendX.plugins").info(
         "Necessary Modules Imported Successfully."
     )
     await userbot.start()
     await Legend.start()
     try:
+        await Legend.stream_decall("https://telegra.ph/file/de3464aa7d6bfafdd2dc3.mp4")
+    except:
+        pass
+    try:
         await Legend.stream_call(
-            "https://telegra.ph/file/8d5db123638c2f6bb6ce4.mp4"
+            "https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4"
         )
     except NoActiveGroupCall:
         LOGGER("LegendX").error(
-            "[ERROR] - \n\nPlease turn on your Logger Group's Voice Call. Make sure you never close/end voice call in your log group."
+            "[ERROR] - \n\nHey Baby, firstly open telegram and turn on voice chat in Logger Group else fu*k off. If you ever ended voice chat in log group i will stop working and users will fu*k you up."
         )
         sys.exit()
     except:
         pass
     await Legend.decorators()
-    LOGGER("LegendX").info("Music Bot Started Successfully,")
+    LOGGER("LegendX").info("\x41\x6e\x6f\x6e\x58\x20\x4d\x75\x73\x69\x63\x20\x42\x6f\x74\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\x2e\x2e\n\n\x4e\x6f\x77\x20\x64\x72\x6f\x70\x20\x79\x6f\x75\x72\x20\x67\x69\x72\x6c\x66\x72\x69\x65\x6e\x64\'\x73\x20\x6e\x75\x64\x65\x73\x20\x61\x74\x20\x40\x44\x65\x76\x69\x6c\x73\x48\x65\x61\x76\x65\x6e\x4d\x46")
     await idle()
 
 
 if __name__ == "__main__":
     loop.run_until_complete(init())
-    LOGGER("LegendX").info("Stopping Music Bot, (Good Bye Lala)")
+    LOGGER("LegendX").info("Stopping Music Bot...")
